b99094ac179bd6f43f5dc3ce1bebd3b8aa54372a
leodongxu@gmail.com <lee@Leos-MacBook-Pro.local>
2021-01-04 21:49:02 -0500

    add data collecting api and create python enviroment

---
+++ dataCollect/retrieveSP500.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import pandas as pd
+import pickle
+
+with open("sp500tickers.pickle", "rb") as f:
+    tickers = pickle.load(f)
+# This function is used to process the close price of the stock
+def adjustClosePrice(tickerList, start="2019-10-18", end="2020-12-31"):
+    df = pd.DataFrame()
+    for ticker in tickerList:
+        adjPrice = yf.download(ticker, start, end)[["Adj Close"]]
+        adjPrice = adjPrice.rename(columns={"Adj Close": ticker})
+        if (df.shape == (0,0)):
+            df = adjPrice
+        else:
+            df = pd.merge(df, adjPrice, how = "outer", left_index=True, right_index=True)
+    return df.round(2)
+
+
+print(tickers)

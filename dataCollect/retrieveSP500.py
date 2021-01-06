413eb2df87215c380b5694673c02803aa56dc92a
leodongxu@gmail.com <lee@Leos-MacBook-Pro.local>
2021-01-05 20:36:46 -0500

    sample output of retrieved data

--- dataCollect/retrieveSP500.py
+++ dataCollect/retrieveSP500.py
@@ -2,6 +2,10 @@ import yfinance as yf
 import pandas as pd
 import pickle
 
+
+'''
+    For now, we only consider the stock prices, we could addin supplimentary information later.
+'''
 with open("sp500tickers.pickle", "rb") as f:
     tickers = pickle.load(f)
 # This function is used to process the close price of the stock
@@ -17,4 +21,5 @@ def adjustClosePrice(tickerList, start="2019-10-18", end="2020-12-31"):
     return df.round(2)
 
 
-print(tickers)
+stock = yf.download(tickers[0], start="2019-10-18", end="2020-12-31")
+print(stock.head(5))
